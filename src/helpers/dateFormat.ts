140831f3a802dacda9e483ead2519db7116835dc
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2023-08-24 00:29:27 -0600

    Se agregó Zona Horaria a la funcion dateFormat

--- src/helpers/dateFormat.ts
+++ src/helpers/dateFormat.ts
@@ -1,14 +1,14 @@
 
 
-
 export const dateFormat = ( date:Date ):string => {
     
-    const longDate = new Date( date )
+    const longDate = new Date(date)
         .toLocaleDateString('es-ES',{
             weekday: 'long',
             year: 'numeric',
             month: 'long',
-            day: 'numeric'
+            day: 'numeric',
+            timeZone: 'UTC' // Establece la zona horaria a UTC
         })
     
     return longDate.substring(0,1).toUpperCase() + longDate.substring(1)
