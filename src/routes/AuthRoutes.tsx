118900fb96c0fc91fdee39f7167152b440765590
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-02-12 02:35:05 -0600

    Rutas de AuthRoutes actualizadas

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -15,10 +15,10 @@ export const AuthRoutes = () => {
         <Routes>
             <Route path="/" element={<AuthLayout />} >
                 <Route index element={<LoginPage />} />
-                <Route path="registrar" element={<RegisterPage />} />
-                <Route path="olvide-password" element={<ForgotPasswordPage />} />
-                <Route path="olvide-password/:token" element={<NewPasswordPage />} />
-                <Route path="confirmar-cuenta/:token" element={<ConfirmAccountPage />} />
+                <Route path="/registrar" element={<RegisterPage />} />
+                <Route path="/olvide-password" element={<ForgotPasswordPage />} />
+                <Route path="/olvide-password/:token" element={<NewPasswordPage />} />
+                <Route path="/confirmar-cuenta/:token" element={<ConfirmAccountPage />} />
                 <Route path="/*" element={<Navigate to="/" />} />
             </Route>
         </Routes>
